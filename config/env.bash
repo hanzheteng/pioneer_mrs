1993e26ae05278885f9dfc2dc0d3716252aab28e
Hanzhe Teng <hanzheteng@outlook.com>
2018-01-03 13:00:14 -0800

    update env loader

--- config/env.bash
+++ config/env.bash
@@ -1,5 +1,5 @@
-#!/bin/bash
-source /opt/ros/kinetic/setup.bash
-source ~/thz_ws/devel/setup.bash
+#!/usr/bin/env bash
+. /opt/ros/kinetic/setup.bash
+. ~/thz_ws/devel/setup.bash
 export ROS_MASTER_URI=http://hzteng:11311
 exec "$@" #replace the current parent process with the new child process
