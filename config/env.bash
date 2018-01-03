996d958a3b3c031449096cfb363689d622c48d0d
Hanzhe Teng <hanzheteng@outlook.com>
2018-01-03 14:08:11 -0800

    env.bash test OK; hostname must be the same

--- config/env.bash
+++ config/env.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-. /opt/ros/kinetic/setup.bash
-. ~/thz_ws/devel/setup.bash
+source /opt/ros/kinetic/setup.bash
+source ~/thz_ws/devel/setup.bash
 export ROS_MASTER_URI=http://hzteng:11311
 exec "$@" #replace the current parent process with the new child process
